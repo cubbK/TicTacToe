65a5e2754bbc39652f7608a65ba117352b1d6f3c
cubbic <danpopa1998@gmail.com>
2016-07-31 17:44:11 +0300

    Ramas de adaugat controlul de cistig

--- logic.js
+++ logic.js
@@ -38,20 +38,37 @@ $( document ).ready(function() {
     })
 
     function AiMove() {
-      var posToBlock = checkToBlock();
+      var posToBlock = checkTwoBlocks(jucator);
+      var posToWin = checkTwoBlocks(ai);
 
-      if (posToBlock !=undefined){
+      if (posToWin !=undefined){
+        var posId = tableConverter[posToWin[0]][posToWin[1]];
+        posId = posId.toString();
+        placeElement(posId,ai);
+      }else if (posToBlock !=undefined){
         var AiId = tableConverter[posToBlock[0]][posToBlock[1]];
         AiId = AiId.toString();
         placeElement(AiId,ai);
 
+      }else if (returnCorner() != undefined){
+        var corner = returnCorner();
+        var cornerId = tableConverter[corner[0]][corner[1]];
+        cornerId = cornerId.toString();
+        placeElement(cornerId,ai);
+      }else if (returnNonCorner() != undefined){
+        var nonCorner = returnNonCorner();
+        var nonCornerId = tableConverter[nonCorner[0]][nonCorner[1]];
+        nonCornerId = nonCornerId.toString();
+        placeElement(nonCornerId,ai);
+      }else {
+        console.log('Gata');
       }
-      console.log(table[0][2]);
+
     }
-    function checkToBlock() {
-      var check1 = checkHorizontally();
-      var check2 = checkVertically();
-      var check3 = checkDiagonally();
+    function checkTwoBlocks(who) {
+      var check1 = checkHorizontally(who);
+      var check2 = checkVertically(who);
+      var check3 = checkDiagonally(who);
       if (check1 !=undefined){
         return check1;
       }else if(check2 != undefined){
@@ -60,11 +77,11 @@ $( document ).ready(function() {
         return check3;
       }
     }
-    function checkHorizontally() {
+    function checkHorizontally(who) {
       for (var i = 0 ; i <3 ; i++){
         var count = 0;
         for (var j = 0 ; j <3;j++){
-          if (table[i][j]==jucator ){
+          if (table[i][j]==who ){
             count++;
           }
         }
@@ -82,11 +99,11 @@ $( document ).ready(function() {
       }
       return undefined;
     }
-    function checkVertically() {
+    function checkVertically(who) {
       for (var j = 0 ;j<3;j++){
         var count = 0;
         for (var i = 0 ; i <3;i++){
-          if (table[i][j]==jucator ){
+          if (table[i][j]==who ){
             count++;
           }
         }
@@ -103,11 +120,11 @@ $( document ).ready(function() {
       }
       return undefined;
     }
-    function checkDiagonally() {
+    function checkDiagonally(who) {
       //check diagonala principala
       var countPrincipal = 0;
       for (var i = 0;i <3 ;i++){
-        if (table[i][i] ==jucator){
+        if (table[i][i] ==who){
           countPrincipal++;
         }
         if (countPrincipal ==2){
@@ -125,7 +142,7 @@ $( document ).ready(function() {
       var countSecundar = 0;
       var j = 2;
       for (var i = 0 ; i <3;i++){
-        if (table[i][j] == jucator){
+        if (table[i][j] == who){
           countSecundar++;
         }
         j--;
@@ -142,6 +159,31 @@ $( document ).ready(function() {
       }
       //end check diagonala ssecundara
       return undefined;
+    }
+
+    function returnCorner() {
+      var corners = [ [0,0] , [0,2] , [2,0] , [2,2]];
+      corners = corners.filter(function (corner) {
+        if (table[corner[0]][corner[1]] == undefined){
+          return true;
+        }else {
+          return false;
+        }
+      });
+      var choosenCorner = corners.randomFromList();
+      return choosenCorner;
+    }
+    function returnNonCorner() {
+      var nonCorners = [ [0,1] , [1,0] , [1,1] , [1,2] , [2,1] ];
+      nonCorners = nonCorners.filter(function (obj) {
+        if (table[obj[0]][obj[1]] == undefined){
+          return true;
+        }else {
+          return false;
+        }
+      });
+      var choosenNonCorner = nonCorners.randomFromList();
+      return choosenNonCorner;
     }
 
 
@@ -163,3 +205,7 @@ $( document ).ready(function() {
 
 
 });
+// prototype def
+Array.prototype.randomFromList = function(){
+  return this[Math.floor(Math.random()*this.length)];
+}
